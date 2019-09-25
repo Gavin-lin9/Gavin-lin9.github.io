702062a2dc5521065fa4ccc3b605af0d449fbd1e
Gavin-lin9 <381342085@qq.com>
2019-09-26 00:05:15 +0800

    Update register.php

--- api/register.php
+++ api/register.php
@@ -4,32 +4,33 @@
     $user = $_GET['username'];
     $pw = $_GET['password'];
 
-    include "./03_conn.php";
-    //  1. 检查用户名是否存在
-    $sql = "
-        SELECT `id` FROM `tb1`
-        WHERE `tb1`.`username`='$user'
-    ";
+    echo $user;
+    // include "./03_conn.php";
+    // //  1. 检查用户名是否存在
+    // $sql = "
+    //     SELECT `id` FROM `tb1`
+    //     WHERE `tb1`.`username`='$user'
+    // ";
 
-    $ret = $conn->query($sql);
-    if($ret->num_rows > 0) {
-        // 用户名存在
-        echo "用户名已经存在";
-    } else {
-        //  2. 如果不存在，则将该用户名和密码插入到表中
-        $sql = "
-            INSERT INTO `tb1`
-            (`id`, `username`, `password`, `tel`, `age`, `gender`)
-            VALUES
-            (NULL, '$user', '$pw', '13333333333', '18', 'female')
-        ";
-        $ret = $conn->query($sql);
-        if($ret) {
-            //注册成功
-            echo "注册成功";
+    // $ret = $conn->query($sql);
+    // if($ret->num_rows > 0) {
+    //     // 用户名存在
+    //     echo "用户名已经存在";
+    // } else {
+    //     //  2. 如果不存在，则将该用户名和密码插入到表中
+    //     $sql = "
+    //         INSERT INTO `tb1`
+    //         (`id`, `username`, `password`, `tel`, `age`, `gender`)
+    //         VALUES
+    //         (NULL, '$user', '$pw', '13333333333', '18', 'female')
+    //     ";
+    //     $ret = $conn->query($sql);
+    //     if($ret) {
+    //         //注册成功
+    //         echo "注册成功";
            
-        } else {
-            //注册失败
-            echo "注册失败";
-        }
-    }
+    //     } else {
+    //         //注册失败
+    //         echo "注册失败";
+    //     }
+    // }
